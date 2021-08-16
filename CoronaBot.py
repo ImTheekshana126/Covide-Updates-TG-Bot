ae21bb1cf7c9e9ed3441214e4a3acac313d2f0c9
Pawan Theekshana <87897206+ImTheekshana126@users.noreply.github.com>
2021-08-16 17:58:19 +0530

    Update CoronaBot.py

--- CoronaBot.py
+++ CoronaBot.py
@@ -26,7 +26,7 @@ def staat(qq):
                 }, 
                 {
                     "text": "🔊 Channel",
-                    "url": "https://t.me/slbotzone"
+                    "url": "https://t.me/Theekshana Official"
                 }
             ]
         ]
