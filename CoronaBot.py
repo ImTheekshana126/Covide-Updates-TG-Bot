fd4ce1a06f36c7bf038f854718cc92ae0eb8200e
Pawan Theekshana <87897206+ImTheekshana126@users.noreply.github.com>
2021-09-03 20:32:22 +0530

    Update CoronaBot.py

--- CoronaBot.py
+++ CoronaBot.py
@@ -4,7 +4,7 @@ import requests
 
 APP_ID= 5011454 #my.telegram.org
 APP_HASH='8d9cf856c343c25a50808cbcfe7de656' #my.telegram.org
-BOTT='1917451466:AAG5F1oT3PS-RI-xAeGYL4qHi0L__GtEbeU'#@botfather
+BOTT='1917451466:AAHss-pbaCVCDrYchEKjS4mEm5tgLUK_Kyg'#@botfather
 
 bot = TelegramClient('bot', APP_ID, APP_HASH).start(bot_token=BOTT)
 
